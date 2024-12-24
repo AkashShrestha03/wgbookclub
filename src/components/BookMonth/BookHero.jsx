7ac9d7a70ef6570eb161fdd88c4c9435487e701a
Akash Shrestha <akashshrestha0399@gmail.com>
2024-12-24 12:48:07 +0530

    Book of the month

--- src/components/BookMonth/BookHero.jsx
+++ src/components/BookMonth/BookHero.jsx
@@ -1,39 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./book.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import axios from "axios";
+import API from "@/config";
 
 const BookHero = () => {
+  const [nominees, setNominees] = useState([]);
+  const router = useRouter(); // Use the router hook
+
+  useEffect(() => {
+    // Fetch data from the API
+    const fetchNominees = async () => {
+      try {
+        const response = await axios.get(
+          `${API}/api/Book/Won/Get/Winner/Admin`
+        ); // Use axios to fetch data
+        const data = response.data; // Access the data from the response
+        if (data.status === 1 && data.statusCode === 200) {
+          setNominees(data.data[0]); // Set nominees from API response
+        } else {
+          console.error("Error fetching nominees:", data.message);
+        }
+      } catch (error) {
+        console.error("Error fetching nominees:", error);
+      }
+    };
+
+    fetchNominees();
+  }, []);
+
+  const handleReadMore = (id) => {
+    router.push(`/readmore?id=${id}`); // Navigate with the ID as a query parameter
+  };
   return (
     <div className={styles["book-container"]}>
       <section>
         <div className={`container ${styles["book-inner"]}`}>
           <div className={` d-flex flex-column  ${styles.story}`}>
             <h4>Book of the Month</h4>
-            <h1>Why Loiter?</h1>
+            <h1>{nominees.title}</h1>
             <span>
               <strong>Author: </strong>
-              Shilpa Phadke, Sameera Khan, Shilpa Ranade
+              {nominees?.AuthorName}
             </span>
-            <p>
-              Lorem ipsum dolor sit amet, ut dicat euismod invidunt pro, ne his
-              dolorum molestie reprehendunt, quo luptatum evertitur ex. Altera
-              integre suavitate per an, alienum phaedrum te sea. Ex sea causae
-              dolores, nam et doming dicunt feugait. Ea mel scripta aperiri
-              postulant. Ut sed affert audire.
-            </p>
-            <button>
-              <Link href={"/readmore"}>Read it here</Link>
+            {/* <p>
+             {nominees?.review}
+            </p> */}
+            <button className="mt-2" onClick={() => handleReadMore(nominees?.BookID)}>
+              Read it here
             </button>
           </div>
           <div
             className={`d-flex justify-content-center align-items-center ${styles.image}`}
           >
-            <Image
-              src={"/img/monthpick/pick-image.png"}
-              height={498}
-              width={360}
-            />
+            <Image src={nominees?.BookCover} height={498} width={360} />
           </div>
         </div>
       </section>
--- src/components/BookMonth/OtherNominees.jsx
+++ src/components/BookMonth/OtherNominees.jsx
@@ -10,17 +10,13 @@ const OtherNominees = () => {
   const router = useRouter(); // Use the router hook
 
   useEffect(() => {
-    // Fetch data from the API
     const fetchNominees = async () => {
       try {
-        const response = await axios.get(
-          `${API}/api/Book/Won/Get/Winner/Admin`
-        ); // Use axios to fetch data
-        const data = response.data; // Access the data from the response
-        if (data.status === 1 && data.statusCode === 200) {
-          setNominees(data.data); // Set nominees from API response
-        } else {
-          console.error("Error fetching nominees:", data.message);
+        const response = await axios.get(`${API}/api/Book/Vote/Percent`);
+        const data = response.data;
+        if (data.status === 1 && data.data?.books) {
+          const updatedNominees = data.data.books;
+          setNominees(updatedNominees);
         }
       } catch (error) {
         console.error("Error fetching nominees:", error);
@@ -39,29 +35,33 @@ const OtherNominees = () => {
       <div className="py-3">
         <h1 className="text-center">Previous Book Of The Month</h1>
       </div>
-      <section className="d-flex justify-content-center flex-wrap gap-5">
-        {nominees.map((nominee) => (
-          <div
-            key={nominee._id}
-            className={`d-flex flex-column gap-1 align-items-center ${styles.book}`}
-          >
-            <Image
-              src={nominee.BookCover}
-              alt={nominee.Booktitle}
-              width={320}
-              height={410}
-            />
-            <h4>{nominee.title}</h4>
-            <p className="fw-bold">{nominee.AuthorName}</p>
-            <p className="text-muted">
-              {nominee.Month}, {nominee.Year}
-            </p>
-            <button onClick={() => handleReadMore(nominee?.BookID)}>
-              Read it here
-            </button>
-          </div>
-        ))}
-      </section>
+      {!nominees?.length ? (
+        <h3 className="text-center">No Nominees Yet!</h3>
+      ) : (
+        <section className="d-flex justify-content-center flex-wrap gap-5">
+          {nominees.map((nominee) => (
+            <div
+              key={nominee._id}
+              className={`d-flex flex-column gap-1 align-items-center ${styles.book}`}
+            >
+              <Image
+                src={nominee.BookCover}
+                alt={nominee.Booktitle}
+                width={320}
+                height={410}
+              />
+              <h4>{nominee.title}</h4>
+              <p className="fw-bold">{nominee.AuthorName}</p>
+              <p className="text-muted">
+                {nominee.Month}, {nominee.Year}
+              </p>
+              <button onClick={() => handleReadMore(nominee?.BookID)}>
+                Read it here
+              </button>
+            </div>
+          ))}
+        </section>
+      )}
     </div>
   );
 };
